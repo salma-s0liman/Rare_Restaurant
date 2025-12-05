ff99d3b8772977ca723606f9dd07bde005dcb09a
SuhaibGhattass21 <ghattasssuhaib89004@gmail.com>
2025-12-05 21:00:21 +0200

    fix: update restaurantAdmin role to use restaurantAdminRoleEnum in entity and service

--- src/DB/entity/restaurantAdmin.ts
+++ src/DB/entity/restaurantAdmin.ts
@@ -9,16 +9,15 @@ import {
 import { Restaurant } from "./restaurant";
 import { User } from "./user";
 import { v4 as uuidv4 } from "uuid";
-// import { restaurantAdminRoleEnum } from "../../common/enums/restaurant.enum";
-import { userRoleEnum } from "../../common/enums/user.enum";
+import { restaurantAdminRoleEnum } from "../../common/enums/restaurant.enum";
 
 @Entity({ name: "restaurant_admins" })
 export class RestaurantAdmin {
   @PrimaryGeneratedColumn("uuid")
   id: string = uuidv4().toString();
 
-  @Column({ type: "enum", enum: userRoleEnum, default: userRoleEnum.staff })
-  role!: userRoleEnum;
+  @Column({ type: "enum", enum: restaurantAdminRoleEnum, default: restaurantAdminRoleEnum.staff })
+  role!: restaurantAdminRoleEnum;
 
   @CreateDateColumn({type: "timestamp", nullable: true})
   created_at?: Date;
